f33be1d3db60b49646fa9b6ae2543909fdeb4bc5
DismissedLight <1686188646@qq.com>
2024-11-28 16:29:52 +0800

    code style

--- src/Snap.WinUI.FrameworkTheming/Snap.WinUI.FrameworkTheming/FrameworkTheming.cs
+++ src/Snap.WinUI.FrameworkTheming/Snap.WinUI.FrameworkTheming/FrameworkTheming.cs
@@ -1,34 +1,39 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: DisableRuntimeMarshalling]
 
 namespace Snap.WinUI.FrameworkTheming;
 
-public static class FrameworkTheming
+public static unsafe partial class FrameworkTheming
 {
-    public static unsafe void SetTheme(Theme theme)
+    private static readonly nint mux = GetModuleHandleW("microsoft.ui.xaml.dll");
+
+    // DXamlInstanceStorage::GetValue(void** phValue)
+    // _Check_return_ HRESULT GetValue(_Outptr_result_maybenull_ Handle* phValue)
+    private static readonly delegate* unmanaged[Stdcall]<DXamlCoreAbi**, int> pDXamlInstanceStorageGetValue = (delegate* unmanaged[Stdcall]<DXamlCoreAbi**, int>)(mux + 0x5ED00C);
+
+    // FrameworkTheming::OnThemeChanged(FrameworkTheming* this, bool forceUpdate)
+    // _Check_return_ HRESULT OnThemeChanged(bool forceUpdate = false)
+    private static readonly delegate* unmanaged[Stdcall]<FrameworkThemingAbi*, bool, int> pFrameworkThemingOnThemeChanged = (delegate* unmanaged[Stdcall]<FrameworkThemingAbi*, bool, int>)(mux + 0x8D3590);
+
+    public static void SetTheme(Theme theme)
     {
         checked
         {
-            nint mux = GetModuleHandleW("microsoft.ui.xaml.dll");
-            nint getDXamlCoreFunc = mux + 0x5ED00C; // DXamlInstanceStorage::GetValue(void** phValue)
-            nint onThemeChangedFunc = mux + 0x8D3590; // FrameworkTheming::OnThemeChanged(FrameworkTheming* this, bool forceUpdate)
+            DXamlCoreAbi* pXamlCore = default;
+            pDXamlInstanceStorageGetValue(&pXamlCore);
 
-            delegate* unmanaged<void**, long> getDXamlCoreDelegate = (delegate* unmanaged<void**, long>)getDXamlCoreFunc;
-            delegate* unmanaged<byte*, byte, long> onThemeChangedDelegate = (delegate* unmanaged<byte*, byte, long>)onThemeChangedFunc;
-
-            void* dxamlCorePtr = (void*)0ul;
-            getDXamlCoreDelegate(&dxamlCorePtr);
-
-            //                                                GetHandle
-            //                                                   GetFrameworkTheming
-            //                                                    v      v
-            byte* themingPtr = *(byte**)(*((ulong*)dxamlCorePtr + 8) + 1648L);
-
-            // m_requestedTheme 
-            themingPtr[80] = (byte)theme;
-            onThemeChangedDelegate(themingPtr, 1);
+            FrameworkThemingAbi* theming = *(FrameworkThemingAbi**)(*((ulong*)pXamlCore + 8) + 1648L);
+            ((Theme*)theming)[80] = theme;
+            pFrameworkThemingOnThemeChanged(theming, true);
         }
     }
-    
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    private static extern nint GetModuleHandleW(string lpModuleName);
+
+    [LibraryImport("kernel32.dll", EntryPoint = "GetModuleHandleW", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
+    private static partial nint GetModuleHandleW(string lpModuleName);
+
+    private readonly struct DXamlCoreAbi;
+
+    private readonly struct FrameworkThemingAbi;
 }
