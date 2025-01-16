0d048f4d242336a52f777c0c77afd6fe83387022
DismissedLight <1686188646@qq.com>
2025-01-16 13:52:11 +0800

    1.6.250108002

--- src/Snap.WinUI.FrameworkTheming/Snap.WinUI.FrameworkTheming/FrameworkTheming.cs
+++ src/Snap.WinUI.FrameworkTheming/Snap.WinUI.FrameworkTheming/FrameworkTheming.cs
@@ -11,27 +11,31 @@ public static unsafe partial class FrameworkTheming
 
     // DXamlInstanceStorage::GetValue(void** phValue)
     // _Check_return_ HRESULT GetValue(_Outptr_result_maybenull_ Handle* phValue)
-    private static readonly delegate* unmanaged[Stdcall]<DXamlCoreAbi**, int> pDXamlInstanceStorageGetValue = (delegate* unmanaged[Stdcall]<DXamlCoreAbi**, int>)(mux + 0x5ED00C);
+    private static readonly delegate* unmanaged[Stdcall]<DXamlCoreAbi**, int> pDXamlInstanceStorageGetValue = (delegate* unmanaged[Stdcall]<DXamlCoreAbi**, int>)(mux + 0x5EE96C);
 
     // FrameworkTheming::OnThemeChanged(FrameworkTheming* this, bool forceUpdate)
     // _Check_return_ HRESULT OnThemeChanged(bool forceUpdate = false)
-    private static readonly delegate* unmanaged[Stdcall]<FrameworkThemingAbi*, bool, int> pFrameworkThemingOnThemeChanged = (delegate* unmanaged[Stdcall]<FrameworkThemingAbi*, bool, int>)(mux + 0x8D3590);
+    private static readonly delegate* unmanaged[Stdcall]<FrameworkThemingAbi*, bool, int> pFrameworkThemingOnThemeChanged = (delegate* unmanaged[Stdcall]<FrameworkThemingAbi*, bool, int>)(mux + 0x8D5040);
 
     public static void SetTheme(Theme theme)
     {
-        checked
-        {
-            DXamlCoreAbi* pXamlCore = default;
-            pDXamlInstanceStorageGetValue(&pXamlCore);
+        DXamlCoreAbi* pXamlCore = default;
+        pDXamlInstanceStorageGetValue(&pXamlCore);
 
-            FrameworkThemingAbi* theming = *(FrameworkThemingAbi**)(*((ulong*)pXamlCore + 8) + 1648L);
-            ((Theme*)theming)[80] = theme;
-            pFrameworkThemingOnThemeChanged(theming, true);
-        }
+        // CCoreServices DirectUI::DXamlServices::GetHandle
+        CCoreServiceAbi* pCoreService = (CCoreServiceAbi*)((ulong*)pXamlCore + 8);
+
+        // CApplication::SetValue
+        // std::unique_ptr<FrameworkTheming> m_spTheming 
+        FrameworkThemingAbi* theming = *(FrameworkThemingAbi**)(*(ulong*)pCoreService + 0x670L);
+        ((Theme*)theming)[0x50] = theme;
+        pFrameworkThemingOnThemeChanged(theming, true);
     }
 
     [LibraryImport("kernel32.dll", EntryPoint = "GetModuleHandleW", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
     private static partial nint GetModuleHandleW(string lpModuleName);
+
+    private readonly struct CCoreServiceAbi;
 
     private readonly struct DXamlCoreAbi;
 
